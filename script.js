8597eaa60a00d914a7ade3dd92a723a2b12501ef
Hitesh Mahale <147713450+Mahalehitesh@users.noreply.github.com>
2026-04-27 23:41:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function mark() {
-  document.getElementById("msg").innerText = "Attendance Marked ✅";
+<script>
+function markAttendance() {
+    const msg = document.getElementById("msg");
+
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    msg.innerHTML = `✅ Attendance Marked <br> 📅 ${date} ⏰ ${time}`;
+    msg.style.color = "#28a745";
 }
+</script>
